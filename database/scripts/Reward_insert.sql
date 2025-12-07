9b7b7b6cedb1cb0c7a0d827f97423fe49f3b3b18
Muhanguzi-boss <bossmuhanguzi@gmail.com>
2025-12-07 20:17:04 +0200

    Add SQL script to insert reward entries

---
+++ database/scripts/Reward_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO REWARDS VALUES (1, 'Free Coffee', 50, 'Redeem for one free coffee');
+INSERT INTO REWARDS VALUES (2, 'Discount Voucher', 100, '10% off next purchase');
+INSERT INTO REWARDS VALUES (3, 'Movie Ticket', 200, 'Free movie ticket');
+INSERT INTO REWARDS VALUES (4, 'Gift Hamper', 500, 'Special gift hamper');
+INSERT INTO REWARDS VALUES (5, 'Shopping Voucher', 1000, 'RWF 20,000 shopping voucher');
